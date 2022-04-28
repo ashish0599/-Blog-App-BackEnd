d98df765add3b5135aadf74508c9662051c568b3
Ashish Khatri <96234443+ashish0599@users.noreply.github.com>
2022-04-28 13:27:37 +0530

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# [Blog App  Server](https://blog-app-08.herokuapp.com/)
+
+# Used Here:
+
+- NodeJS
+- ExpressJS
+- Mongoose
+- Nodemon
---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+
+//routes
+const authRoute = require("./Routes/Auth");
+const userRoute = require("./Routes/Users");
+const postRoute = require("./Routes/Posts");
+const categoryRoute = require("./Routes/Categories");
+app.use(cors());
+app.use(express.json());
+app.get("/", (request, response) => {
+  response.send("Welcome to Mern Blog Application");
+});
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(console.log("connected to mongo db"))
+  .catch((err) => console.log("Error in Db connection", err));
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    console.log(req.body);
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// make public folder:
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    console.log("in upload", req.file);
+    console.log("In image upload");
+    res.status(200).send("file has been uploaded");
+  } catch (err) {
+    res.status(500).send("error in uploading");
+  }
+});
+
+port = process.env.PORT || 4700;
+app.listen(port, () => {
+  console.log("App  running at ", port);
+});
+//middlewares
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+
